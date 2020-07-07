3166366ebcb85ad582d6c6fd2a2c40f832ef9b8d
Stevanus Prasetyo Soemadi <stevanus.pras@gmail.com>
2020-07-07 21:45:42 +0700

    Setup carthage and add new depedencies

---
+++ DuniaDota/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  DuniaDota
+//
+//  Created by Stevanus Prasetyo Soemadi on 07/07/20.
+//  Copyright © 2020 Stevanus Prasetyo Soemadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
